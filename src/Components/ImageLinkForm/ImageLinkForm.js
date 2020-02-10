aa53fa3582197e6d4348b1eaff412c60032590b9
Chrononox <tegbert@gmail.com>
2020-02-10 13:16:36 -0600

    finished logo component

---
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ImageLinkForm = () =>{
+    return(
+        <div>
+            <h1>Hi</h1>
+        </div>
+    )
+}
+
+export default ImageLinkForm;
--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Tilt from 'react-tilt';
 import './Logo.css';
+import logo from './brain3.png';
 
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" options={{ max : 50 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner"> 👽 </div>
+                <div className="Tilt-inner pa3">
+                    <img src={logo} alt='logo'/>   
+                 </div>
             </Tilt>
         </div>
     )
