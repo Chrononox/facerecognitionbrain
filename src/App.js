9fddd5d34edee5b97ede4de29467f2181d097904
Chrononox <tegbert@gmail.com>
2020-02-10 13:59:07 -0600

    added particle background

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import Navigation from './Components/Navigation/Navigation'
-import Logo from './Components/Logo/Logo'
-import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+import Particles from 'react-particles-js';
 import './App.css';
+
+const partivlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density:{
+        enable: true,
+        value_area: 600,
+      }
+    }
+  }
+}
 
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={partivlesOptions} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition /> */}
     </div>
