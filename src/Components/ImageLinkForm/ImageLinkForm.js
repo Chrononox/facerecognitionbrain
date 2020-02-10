956900b499d6ee78b9e37e530b827f13c25e22ff
Chrononox <tegbert@gmail.com>
2020-02-10 13:31:03 -0600

    added form

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './ImageLinkForm.css';
 
 const ImageLinkForm = () =>{
     return(
         <div>
-            <h1>Hi</h1>
+            <p className='f3'>
+                {'This Magic Brain will detect faces in you pictures. Give it a try'}
+            </p>
+            <div className='center'>
+                <div className='form center pa4 br3 shadow-5'>
+                    <input className='f4 pa2 w-70 center' type='text' />
+                    <button className='w30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+                </div>
+            </div>
         </div>
     )
 }
